eb9e03465bacbf26d1c532e6110ea88b94109299
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-10-16 11:48:45 +0200

    Export types

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
