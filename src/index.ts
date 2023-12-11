7c3440feb039bb859151cf4ad5b2064ab07185d0
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2023-12-11 16:41:15 +0100

    feat: export BlocksContent type (#13)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import { BlocksRenderer } from './BlocksRenderer';
 
+import type { RootNode } from './BlocksRenderer';
+
+type BlocksContent = RootNode[];
+
 export { BlocksRenderer };
+export type { BlocksContent };
